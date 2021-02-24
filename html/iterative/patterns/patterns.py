49b3c5eaf43489553966078b443f276c29cb0728
thulasi-reddy <thulasisweety456@gmail.com>
2021-02-24 12:46:42 +0530

    one

---
+++ html/iterative/patterns/patterns.py
@@ -0,0 +1,4 @@
+n=int(input('enter no of rows:'))
+for i in range(n):
+    print(str(i+1) + ' ') * n
+
--- html/iterative/patterns/programms/data types/list.py
+++ html/iterative/patterns/programms/data types/list.py
@@ -1,8 +1,7 @@
-l=[1,2,4,7,4]
-l.append(9)
-l.append(56)
-l.append(45)
-l.append(78)
+l=[1,2,4,5,6,7,2,3,5]
+l.sort(reverse=True)
 print(l)
-l.pop(3)
-print(l)
+
+
+
+
